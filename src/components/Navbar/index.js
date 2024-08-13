7ebb0194949fb54bd98ddc655782590986239d15
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-13 01:20:24 -0400

    Add salon name in upper left corner

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,6 +4,10 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="nav-bar">
+            <div className="salon">
+                <span className="name">Lana's </span>
+                UNISEX Hair Salon
+            </div>
             <nav>
                 <NavLink exact={true} activeClassname="active" to="/">Home</NavLink>
                 <NavLink exact={true} activeClassname="active" to="/services">Services</NavLink>
