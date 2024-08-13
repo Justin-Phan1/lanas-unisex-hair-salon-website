d364a74dba4af34796760e119fe5fc5f4a061e09
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-13 01:44:28 -0400

    Add Reviews page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,6 +13,7 @@ const Navbar = () => {
                 <NavLink exact={true} activeClassname="active" to="/services">Services</NavLink>
                 <NavLink exact={true} activeClassname="active" to="/gallery">Gallery</NavLink>
                 <NavLink exact={true} activeClassname="active" to="/info">Info</NavLink>
+                <NavLink exact={true} activeClassname="active" to="/reviews">Reviews</NavLink>
             </nav>
         </div>
     );
