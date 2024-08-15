a5d68e67b9668babae46a72247f1b88d33cd18a1
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-15 18:21:15 -0400

    Remove Info component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Services from './components/Services';
-import Info from './components/Info';
 import Gallery from './components/Gallery';
 import Reviews from './components/Reviews';
 
@@ -13,7 +12,6 @@ function App() {
       <Route path ="/" element = {<Layout />} >
       <Route index element={<Home />} />
         <Route path="services" element={<Services />} />
-        <Route path="info" element={<Info />} />
         <Route path="gallery" element={<Gallery />} />
         <Route path="reviews" element={<Reviews />} />
       </Route>
---
+++ src/components/Info/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const Info = () => {
+    return (
+        <div className="info text-zone">
+            <h2>Info</h2>
+            <p>
+                At the moment, we operate on a walk-in basis and are not accepting appointments. 
+            </p>
+            <h2>Amenities</h2>
+            <ul>
+                <li>Bathroom</li>
+                <li>Chairs</li>
+                <li>Air Conditioning</li>
+                <li>Heating</li>
+                <li>Complimentary Wi-Fi</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Info;
