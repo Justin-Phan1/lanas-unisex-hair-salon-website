c4fb44df4b99366786d0b6b146f5468e01f2ab31
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-20 00:13:37 -0400

    Remove Gallery component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Services from './components/Services';
-import Gallery from './components/Gallery';
 import Testimonials from './components/Testimonials';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <Route path ="/" element = {<Layout />} >
       <Route index element={<Home />} />
         <Route path="services" element={<Services />} />
-        <Route path="gallery" element={<Gallery />} />
         <Route path="testimonials" element={<Testimonials />} />
       </Route>
     </Routes>
