d79c90240e2e6b8d7c5d6fb9b43dbfe05457ffe2
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2024-08-29 03:16:57 -0400

    Create icons and functions for hamburger menu and X button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,37 @@
 import './index.css'
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons"
+import { useState } from 'react'
 
 const Navbar = () => {
+    const[showNav, setShowNav] = useState(false);
+
     return (
         <div className="nav-bar">
             <div className="salon">
                 <span className="name">Lana's </span>
                 UNISEX Hair Salon
             </div>
-            <nav>
-                <NavLink exact={true} activeClassname="active" to="/">Home</NavLink>
-                <NavLink exact={true} activeClassname="active" to="/services">Services</NavLink>
-                <NavLink exact={true} activeClassname="active" to="/reviews">Reviews</NavLink>
+            <nav className={showNav ? "mobile-show" : ""}>
+                <NavLink exact={true} activeClassname="active" to="/" onClick={() => setShowNav(false)}>Home</NavLink>
+                <NavLink exact={true} activeClassname="active" to="/services" onClick={() => setShowNav(false)}>Services</NavLink>
+                <NavLink exact={true} activeClassname="active" to="/reviews" onClick={() => setShowNav(false)}>Reviews</NavLink>
+                <FontAwesomeIcon
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color="yellow"
+                    size="3x"
+                    className="close-icon"
+                />
             </nav>
+            <FontAwesomeIcon 
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color="yellow"
+                size="3x"
+                className="hamburger-icon"
+            />
         </div>
     );
 }
